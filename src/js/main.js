065db85d0c290c1ed8ddd16caacdb46a6a2eae69
vika-4eredni4enko <victoria.4eredni4enko@gmail.com>
2022-10-15 22:37:50 +0300

    add files

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+console.log('Hello!');
+
+import * as flsFunctions from "./modules/functions.js";
+
+flsFunctions.isWebP();
+
+import Swiper, { Navigation, Pagination } from 'swiper';
+
+ // init Swiper:
+ const swiper = new Swiper();
